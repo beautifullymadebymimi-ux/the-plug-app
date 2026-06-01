94d7c23bc60621e52c05cb4250aab68ca51ffa26
Melissa Cole <melissacole@MelissacBookNeo.attlocal.net>
2026-06-01 17:12:01 -0400

    Increment build number for native crash fixes

--- app.config.ts
+++ app.config.ts
@@ -50,7 +50,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "4",
+    buildNumber: "5",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
@@ -65,7 +65,7 @@ const config: ExpoConfig = {
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
     package: env.androidPackage,
-    versionCode: 4,
+    versionCode: 5,
     permissions: ["POST_NOTIFICATIONS"],
     intentFilters: [
       {
