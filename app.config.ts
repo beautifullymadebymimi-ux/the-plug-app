f264acddc5701a61ff735bca77e86810018ff8c9
Melissa Cole <melissacole@MelissacBookNeo.attlocal.net>
2026-06-01 18:25:33 -0400

    Increment build number for audio and events fixes

--- app.config.ts
+++ app.config.ts
@@ -50,7 +50,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "5",
+    buildNumber: "6",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
@@ -65,7 +65,7 @@ const config: ExpoConfig = {
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
     package: env.androidPackage,
-    versionCode: 5,
+    versionCode: 6,
     permissions: ["POST_NOTIFICATIONS"],
     intentFilters: [
       {
